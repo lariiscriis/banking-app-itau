14e438142767e6a76df7d7caa2d2d58a7d7261da
lariiscriis <larissa.santana.ae2020@gmail.com>
2025-11-16 19:42:31 -0300

    Initial commit

---
+++ src/main/java/com/itau/desafiotecnico/larissa/banking_app_itau/BankingAppItauApplication.java
@@ -0,0 +1,13 @@
+package com.itau.desafiotecnico.larissa.banking_app_itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAppItauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itau/desafiotecnico/larissa/banking_app_itau/BankingAppItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.desafiotecnico.larissa.banking_app_itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
